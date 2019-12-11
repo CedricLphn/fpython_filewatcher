329e17c8d5f1acc9d7a138a038ea3742ab4b13d4
Gustin Florian <florian.gustin.business@gmail.com>
2019-12-11 13:25:31 +0100

    [ADD] -d command

---
+++ src/cli_parser.py
@@ -0,0 +1,25 @@
+import argparse, os
+"""Parser of arguments from cli
+
+"""
+parser = argparse.ArgumentParser(description="Intro à remplir")
+## set exclusive
+group = parser.add_mutually_exclusive_group()
+## set all arguments possibilities
+group.add_argument("-d", "--directory", action="store_true", help="directory name")
+group.add_argument("-i", "--interval", type=int, help="number of seconds")
+group.add_argument("-c", "--config", type=str, help="filename config [interval, directory]")
+group.add_argument("-l", "--logging", type=str, help="config filename")
+
+## get args
+args = parser.parse_args()
+
+## if directory given
+if args.directory:
+    ## change the directory by the argument passed for --directory
+    os.chdir(args.directory)
+    print(os.getcwd())
+
+# if interval given
+if args.interval:
+
