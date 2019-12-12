73f27bdfd18b735d734f20f9e90b48cdf0d66b75
Gustin Florian <florian.gustin.business@gmail.com>
2019-12-12 02:23:46 +0100

    [ADD/EDIT] logging, cli_parser all conditions

---
+++ filewatch.py
@@ -0,0 +1,12 @@
+from src.models.cli_parser import cli_parser
+
+
+def main():
+    """
+
+    """
+    ## parse arguments
+    cli_parser()
+
+if __name__ == '__main__':
+    main()
--- src/models/cli_parser.py
+++ src/models/cli_parser.py
@@ -1,39 +1,51 @@
-from src.load_yaml import load_config
+import argparse
 
-import argparse, os
+from src.models.load_yaml import load_config
+
 
 def cli_parser():
     """Parser of arguments from cli
 
     """
-    parser = argparse.ArgumentParser(description="Intro à remplir")
-    ## set exclusive
-    group = parser.add_mutually_exclusive_group()
+    parser = argparse.ArgumentParser(description="Monotoring of directory contents")
+
     ## set all arguments possibilities
-    group.add_argument("-d", "--directory", action="store_true", help="directory name")
-    group.add_argument("-i", "--interval", type=int, help="number of seconds")
-    group.add_argument("-c", "--config", type=str, help="filename config [interval, directory]")
-    group.add_argument("-l", "--logging", type=str, help="config filename")
+    root_group = parser.add_mutually_exclusive_group()
+    parser.add_argument("-d", "--directory", type=str, help="directory name")
+    parser.add_argument("-i", "--interval", type=int, help="number of seconds")
+    parser.add_argument("-l", "--logging", type=str, help="logging config filename")
+
+    group_list_config = root_group.add_mutually_exclusive_group()
+    group_list_config.add_argument("-c", "--config", type=str, help="[interval int in seconds, directory path] config yaml filename")
 
     ## get args
     args = parser.parse_args()
 
+    ## if logging file config given
+    if args.logging:
+        _logging = args.logging
+        if _logging != "true":
+            print("Logging error : must be set on '-l true' to handle debugs, infos, warnings")
+
+
+    ## if yaml file config given
+    if args.config:
+        ## load YAML config file
+        _config_yaml = load_config(args.config)
+        _interval = _config_yaml['interval']
+        _directory = _config_yaml['directory']
+        print("new path to monitor : ", _directory)
+        print("interval : ", _interval)
+        exit()
+
     ## if directory given
     if args.directory:
         ## change the directory by the argument passed for --directory
-        os.chdir(args.directory)
-        print(os.getcwd())
+        _directory = args.directory
+        print("new path to monitor : ", _directory)
 
     ## if interval given
     if args.interval:
         ## get the interval duration
         _interval = args.interval
-
-    ## if file config given
-    if args.config:
-        ## load YAML config file
-        _config_yaml = load_config('tests/config.yaml')
-
-
-
-
+        print("interval : ", _interval)
