e50d4e99f3acdead118ea57823ce932e77138751
Gustin Florian <florian.gustin.business@gmail.com>
2019-12-12 02:27:00 +0100

    [FIX] small fix

--- filewatch.py
+++ filewatch.py
@@ -1,12 +1,31 @@
 from src.models.cli_parser import cli_parser
 
+import logging
 
 def main():
     """
 
     """
+    _run = False
+    _with_logs = False
     ## parse arguments
-    cli_parser()
+    get_list = cli_parser()
+    if get_list is not None:
+
+        if len(get_list) > 1:
+            ## do thing with monitoring
+            _run = True
+            if len(get_list) == 3:
+                _with_logs = True
+
+    if _run is True:
+        ## continue
+        condition = _with_logs == True
+        msg = ", logs writing" if condition else ", logs off"
+        print("lets go", msg)
+
+
+
 
 if __name__ == '__main__':
     main()
