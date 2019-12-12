7ce3dbfc84eeab509ab7adb4f64ad93d8a95dc4a
Gustin Florian <florian.gustin.business@gmail.com>
2019-12-12 15:21:24 +0100

    [EDIT] DocStrings, comments

--- src/checksum.py
+++ src/checksum.py
@@ -1,6 +1,10 @@
 import hashlib
 
+
 def checksum_file(filename):
+    """Create checksum of a file
+    :return: sha256 string
+    """
     sha256 = hashlib.sha256()
     with open(filename, "rb") as f:
         for block in iter(lambda: f.read(4096), b""):
